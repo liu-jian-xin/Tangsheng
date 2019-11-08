496d84cdae0517860c09d2896249e09306827ff9
liu-jian-xin <57519234+liu-jian-xin@users.noreply.github.com>
2019-11-08 20:18:55 +0800

    update

--- src/main/java/com/atguigu/xiyou/TangSeng.java
+++ src/main/java/com/atguigu/xiyou/TangSeng.java
@@ -4,6 +4,7 @@ public class TangSeng {
      public void getBook() {
     	 System.out.println("悟空救我");
       System.out.println("悟空救我");
+       System.out.println("悟空救我");
     	 new JinGuBang().beat();
      }
 }
